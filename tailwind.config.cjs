f9e90c5a6c649b62bb68739e1ec25104265fdcbc
Ryan Washburne <ryan.washburne@gmail.com>
2023-06-25 23:02:39 -0700

    added font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Moderat", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
 };
