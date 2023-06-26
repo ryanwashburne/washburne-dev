ed80274d781da98801617e957e5d152e1d81a303
Ryan Washburne <ryan.washburne@gmail.com>
2023-06-25 20:00:01 -0700

    polish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), image()]
+  integrations: [tailwind(), image()],
 });
